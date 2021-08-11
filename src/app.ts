785990b6e8c3c9485dbc0c609d6c9a72d5bbc263
Allen Sarmiento <allensarmiento3@gmail.com>
2021-08-11 12:15:00 -0700

    Add ping route

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,12 @@ import 'express-async-errors';
 
 import { NotFoundError } from './errors/not-found-error';
 
+import { pingRouter } from './routes/ping';
+
 const app = express();
 app.use(json());
+
+app.use(pingRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
