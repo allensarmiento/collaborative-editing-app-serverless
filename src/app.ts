9ae868c2713c1e309920dd689b41efcad7a494bd
Allen Sarmiento <allensarmiento3@gmail.com>
2021-08-11 12:12:35 -0700

    Add not found error and apply to app

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,13 @@ import { json } from 'body-parser';
 import express from 'express';
 import 'express-async-errors';
 
+import { NotFoundError } from './errors/not-found-error';
+
 const app = express();
 app.use(json());
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
 
 export { app };
